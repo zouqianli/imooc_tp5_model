b3c802f0bd089a5ee6ea35db73b8e64f157a5917
zouqianli <1004248149@qq.com>
2017-09-05 18:14:54 +0800

    模型聚合操作 数据准备:$this->model_saveAll();

--- tp5_model/app/index/controller/Index.php
+++ tp5_model/app/index/controller/Index.php
@@ -1,10 +1,31 @@
 <?php
 namespace app\index\controller;
-use think\Db;
+use app\index\model\User;
 class Index
 {
     public function index()
     {
+        $this->model_saveAll();
+    }
 
+    /**
+     * 数据准备
+     */
+    public function model_saveAll()
+    {
+        $user = new User();
+        $data = [];
+        for($i=1;$i<21;$i++)
+        {
+            $data[] = [
+                'name'  =>  '邹前立'."{$i}",
+                'email' =>  '1004248149@qq.com'."{$i}",
+                'password'  =>  md5('123456'),
+                'num'   =>  '100'+"{$i}",
+                'mygroup' =>  $i%4,
+            ];
+        }
+        $res = $user->saveAll($data);
+        dump($res);
     }
 }
