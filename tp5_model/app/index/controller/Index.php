4b0854b065044a8d8db17a1689fb9c67b6a8a408
zouqianli <1004248149@qq.com>
2017-09-06 00:51:11 +0800

    模型修改器 不使用:和普通添加数据一样 使用:对值进行处理,然后插入表

--- tp5_model/app/index/controller/Index.php
+++ tp5_model/app/index/controller/Index.php
@@ -1,10 +1,27 @@
 <?php
 namespace app\index\controller;
-use think\Db;
+use app\index\model\User;
 class Index
 {
     public function index()
     {
+        $this->mode_setFiledAttr();
+    }
 
+    /**
+     * 修改器
+     * 不使用:和普通添加数据一样
+     * 使用:对值进行处理,然后插入表
+     */
+    public function mode_setFiledAttr()
+    {
+        $res = User::create([
+            'name'  =>  '作用修改器',
+            'email' =>  '666@qq.com',
+            'sex'   =>  '2',
+            'password'  => '123456',
+            'mygroup'   =>  1,
+            'num'   =>  10,
+        ]);
     }
 }
--- tp5_model/app/index/model/User.php
+++ tp5_model/app/index/model/User.php
@@ -9,5 +9,15 @@ namespace app\index\model;
 use think\Model;
 class User extends Model
 {
-
+    /**
+     * 修改器--密码为例
+     * @param $val 某个变量(表中对应字段)
+     * @param $data 所有变量(所有字段)--获取器也有这个参数
+     * @return string 返回处理过后的值--设置字段值
+     */
+    public function setPasswordAttr($val,$data)
+    {
+        //dump(md5($val.$data['name']));
+        return md5($val);
+    }
 }
