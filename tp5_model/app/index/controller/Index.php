3564e27ad01d6644d8c70c78e7840304bf257080
zouqianli <1004248149@qq.com>
2017-09-05 16:23:06 +0800

    模型更新 数据准备 class Index {     public function index()     {         //$this->model_saveAll();//准备数据     } }

--- tp5_model/app/index/controller/Index.php
+++ tp5_model/app/index/controller/Index.php
@@ -1,10 +1,31 @@
 <?php
 namespace app\index\controller;
-use think\Db;
+use app\index\model\User;
 class Index
 {
     public function index()
     {
+        //$this->model_saveAll();//准备数据
+    }
 
+    /**
+     * saveAll准备数据
+     */
+    public function model_saveAll()
+    {
+        $user = model('User');
+        $data = [];
+        for ($i = 1; $i < 21; $i++)
+        {
+            $data[] = [
+                'name'  =>  '邹前立'."{$i}",
+                'email' =>  '1004248149@qq.com_'."{$i}",
+                'password'   =>  md5('123456'),
+                'num'   =>  100+"{$i}",
+                'mygroup' =>  $i%4,
+            ];
+        }
+        $res = $user->saveAll($data);
+        dump($res);
     }
 }
