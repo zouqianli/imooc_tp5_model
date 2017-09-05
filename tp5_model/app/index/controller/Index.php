e2e0e4b5efeced93a118eed0115f1a4d5ec27863
zouqianli <1004248149@qq.com>
2017-09-05 19:05:20 +0800

    模型聚合操作 #使用模型类::静态方法(max|min|sum|avg|count)--5.0.10测试时警告:非静态方法不要使用静态方式调用 #使用对象[->where]->聚合方法(max|min|sum|avg|count) #都可以使用where

--- tp5_model/app/index/controller/Index.php
+++ tp5_model/app/index/controller/Index.php
@@ -1,11 +1,91 @@
 <?php
 namespace app\index\controller;
+use think\Controller;
 use app\index\model\User;
-class Index
+class Index extends Controller
 {
     public function index()
     {
-        $this->model_saveAll();
+        $this->model_count();
+        $this->model_sum('num');
+        $this->model_max('num');
+        $this->model_min('num');
+        $this->model_avg('num');
+
+        $this->model_static();
+
+        //$this->model_saveAll();
+    }
+
+    #都可以使用where
+    /**
+     * 记录数
+     */
+    public function model_count()
+    {
+        $user = new User();
+        $res = $user->where('id','<','3')->count();
+        dump($res);
+    }
+
+    /**
+     * 和
+     * @param string $field 求和的字段
+     */
+    public function model_sum($field='')
+    {
+        $user = new User();
+        $res = $user->sum($field);
+        dump($res);
+    }
+
+    /**
+     * 最大值
+     * @param string $field 最大值字段
+     */
+    public function model_max($field='')
+    {
+        $user = new User();
+        $res = $user->max($field);
+        dump($res);
+    }
+
+    /**
+     * 最小值
+     * @param string $field 最小值字段
+     */
+    public function model_min($field='')
+    {
+        $user = new User();
+        $res = $user->min($field);
+        dump($res);
+    }
+
+    /**
+     * 平均值
+     * @param string $field 平均值字段
+     */
+    public function model_avg($field='')
+    {
+        $user = new User();
+        $res = $user->avg($field);
+        dump($res);
+    }
+
+    #静态方式调用
+    #警告:非静态方法不要使用静态方式调用
+    public function model_static()
+    {
+        $res = User::where('id','<','3')->count();
+        dump($res);
+        $res = User::sum('num');
+        dump($res);
+        $res = User::max('num');
+        dump($res);
+        $res = User::min('num');
+        dump($res);
+        $res = User::avg('num');
+        dump($res);
     }
 
     /**
