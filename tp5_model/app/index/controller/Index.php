1bb76dbe6cf148df6b005c8fbac2393d7991a445
zouqianli <1004248149@qq.com>
2017-09-06 13:36:14 +0800

    模型软删除 namespace app\index\model; use think\Model; use traits\model\SoftDelete; class User extends Model {     use SoftDelete;     protected $deleteTime = 'delete_time';//软删除标记字段 默认为int(NULL) } 默认情况下查询的数据不包含软删除数据 包含查询软删除的数据 User::withTrashed()->find(); User::withTrashed()->select(); 只查询软删除的数据 User::onlyTrashed()->find(); User::onlyTrashed()->select();

--- tp5_model/app/index/controller/Index.php
+++ tp5_model/app/index/controller/Index.php
@@ -5,9 +5,67 @@ class Index
 {
     public function index()
     {
-        $this->model_create();
+//        $this->model_create();
+//        $this->model_softDelete();
+//        $this->model_softDestroy();
+//        $this->model_softSelect();
+    }
+/** 软删除 */
+    /**
+     * 类方法 destroy
+     *
+     * 模型类 use traits\model\SoftDelete;
+     * use SoftDelete;
+     * 默认delete_time字段为int(null)
+     *
+     * SELECT * FROM `tp5_user` WHERE `id` = 2
+     * 表中执行了更新操作
+     * UPDATE `tp5_user` SET `delete_time`=1504674082,`update_at`=1504674082 WHERE `id` = 2
+     */
+    public function model_softDestroy()
+    {
+        $res = User::destroy(2);//软删除
+        //$res = User::destroy(2,true);//真实删除
+        dump($res);
     }
 
+    /**
+     * 对象方法 delete
+     * delete_time字段允许为空,不然报错
+     *
+     * 模型类 use traits\model\SoftDelete;
+     * use SoftDelete;
+     * 默认delete_time字段为int(null)
+     *
+     * SELECT * FROM `tp5_user` WHERE ( `id` = 3 ) AND `tp5_user`.`delete_time` IS NULL LIMIT 1
+     * UPDATE `tp5_user` SET `delete_time`=1504675389,`update_at`=1504675389 WHERE `id` = 3
+     */
+    public function model_softDelete()
+    {
+        $user = User::get(3);
+        $res = $user->delete(); //软删除
+//        $res = $user->delete(true);//真实删除
+        dump($res);
+    }
+
+    /**
+     * 使用软删除后的查询
+     * 默认情况下查询的数据不包含软删除数据
+     * 如果需要包含软删除的数据
+     * User::withTrashed()->find();
+     * User::withTrashed()->select();
+     * 如果只查询软删除的数据
+     * User::onlyTrashed()->find();
+     * User::onlyTrashed()->select();
+     */
+    public function model_softSelect()
+    {
+        $res = User::onlyTrashed()->find();
+//        $res = User::onlyTrashed()->select();
+        dump($res->toArray());
+    }
+
+/** 自动完成 */
     /**
      * 1.修改配置文件 不推荐(全局有效,如果有的表没有时间戳字段报错)
      * 自动写入时间戳字段(添加时间|更新时间)
--- tp5_model/app/index/model/User.php
+++ tp5_model/app/index/model/User.php
@@ -7,9 +7,13 @@
  */
 namespace app\index\model;
 use think\Model;
+use traits\model\SoftDelete;
 class User extends Model
 {
     protected $autoWriteTimestamp = true;
     protected $createTime = false;
     protected $updateTime = 'update_at';
+
+    use SoftDelete; // 使用traits
+    protected $deleteTime = 'delete_time';//指定软删除字段
 }
